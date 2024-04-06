5c326bcf7cb59e68b5647fe4a358a0b6a2189511
Beveky <im.beveky@gmail.com>
2024-04-07 00:51:32 +0100

    deployment

--- components/MobileMenu.tsx
+++ components/MobileMenu.tsx
@@ -1,35 +1,54 @@
 import React from 'react';
-
+import { useRouter } from 'next/router';
 interface MobileMenuProps {
   visible?: boolean;
 }
 
 const MobileMenu: React.FC<MobileMenuProps> = ({ visible }) => {
+  const router = useRouter();
+
   if (!visible) {
     return null;
   }
 
+  const redirectToWishlist = () => {
+    router.push('/wishlist');
+  };
+  const redirectToHome = () => {
+    router.push('/');
+  };
+  const redirectToNewest = () => {
+    router.push('/newest');
+  };
+
   return (
     <div className="bg-black w-56 absolute top-8 left-0 py-5 flex-col border-2 border-gray-800 flex">
       <div className="flex flex-col gap-4">
+
+        <button onClick={redirectToHome}>
         <div className="px-3 text-center text-white hover:underline">
           Home
         </div>
+        </button>
         <div className="px-3 text-center text-white hover:underline">
           Series
         </div>
         <div className="px-3 text-center text-white hover:underline">
           Films
         </div>
+
+        <button onClick={redirectToNewest}>
         <div className="px-3 text-center text-white hover:underline">
           New & Popular
         </div>
+        </button>
+
+        <button onClick={redirectToWishlist}>
         <div className="px-3 text-center text-white hover:underline">
           My List
         </div>
-        <div className="px-3 text-center text-white hover:underline">
-          Browse by Languages
-        </div>
+        </button>
+  
       </div>
     </div>
   )
